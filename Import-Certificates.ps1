f6fc630b652ca3e603ca5d93bd0347d3f4e25eca
Pavlo Chavyr <pavlo.chavyr@nixs.com>
2025-11-25 15:56:28 +0200

    Added Import-Certificates.ps1 script

---
+++ Import-Certificates.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    Import multiple certificates to Certificate Store
+.DESCRIPTION
+    This script installs imports certificates to Certificate Stor so OS can trust them.
+.PARAMETER Certificates
+    The array of certificates content in PEM format
+.PARAMETER CertStoreLocation
+    Certificate Store Location (default: 'Cert:\LocalMachine\Root')
+.PARAMETER FilePath
+    File Path for temporary certificate file storage (default: 'C:\temp\CertToImport.cer')
+.EXAMPLE
+    .\Import-Certificates.ps1 -Certificates "$firstCert","$secondCert"
+#>
+
+param(
+  [array]$Certificates = "",
+  [string]$CertStoreLocation = 'Cert:\LocalMachine\Root'
+  [string]$FilePath = 'C:\temp\CertToImport.cer',
+)
+
+foreach ($Certificate in $Certificates) {
+  Write-Host "Importing Certificate to $CertStoreLocation"
+  $Certificate | Out-File -FilePath $FilePath
+  Import-Certificate -FilePath $FilePath -CertStoreLocation $CertStoreLocation
+  Remove-Item -Path $FilePath
+  Write-Host "Import is done"
+}
